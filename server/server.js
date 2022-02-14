bee8c5f6c89901fca9bdae91a45b1fbbc4d46559
Gwack <gracalynntrammell@gmail.com>
2022-02-13 18:48:00 -0600

    created third server to start client/server databses

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 
+const path = require('path');
+
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -31,6 +33,15 @@ const startServer = async () => {
 
 // Initialize the Apollo server
 startServer();
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
